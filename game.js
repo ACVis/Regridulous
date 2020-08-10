d111db6daf3e7a8f416b774832aa95daaaf80e6f
Alex C <alex.caldwell@reason.org>
2020-08-10 18:57:26 -0400

    More State Sketching

--- game.js
+++ game.js
@@ -9,7 +9,68 @@ import images from "./assets/*.png";
 import tileset from "./assets/tileset/*.png";
 import { TextSprite } from "phaser-ui-tools";
 
+class State {
+    name = "DefaultState";
+    constructor({ debug = false } = {}) {
+        this.debug = debug;
+    }
+
+    handleInput(subject, input) {}
+
+    update(subject) {}
+
+    enter(subject) {
+        if (this.debug) {
+            console.log("Entered " + this.name ? this.name : "Unnamed State");
+        }
+    }
+    exit() {}
+}
+
+class DefaultState extends State {
+    handleInput(keyevent) {
+        this.player.handleInput(keyevent);
+        // console.log("this in handleInput", this);
+        // const { code } = keyevent;
+
+        // if (code === "ArrowRight") {
+        //     // this.takeTurn();
+        //     this.player.x += CST.TILE_SIZE;
+        //     console.log(this.player.width, this.player.displayWidth);
+
+        //     console.log("right up!");
+        // } else if (code === "ArrowDown") {
+        //     // this.takeTurn();
+        //     this.player.y += CST.TILE_SIZE;
+
+        //     console.log("down up!");
+        // } else if (code === "ArrowLeft") {
+        //     // this.takeTurn();
+        //     this.player.x -= CST.TILE_SIZE;
+        //     console.log("right up!");
+        // } else if (code === "ArrowUp") {
+        //     // this.takeTurn();
+        //     this.player.y -= CST.TILE_SIZE;
+
+        //     console.log("down up!");
+        // }
+    }
+}
 class mainScene {
+    constructor() {
+        this.state = new DefaultState();
+    }
+    handleInput(input) {
+        const state = this.state.handleInput(input);
+
+        if (state != null) {
+            this.state = state;
+            this.state.enter(this);
+        }
+
+        this.state.handleInput(input);
+        // this.equipment.handleInput(input);
+    }
     preload() {
         this.load.image("player", images.player);
         this.load.image("enemy_basic", images.enemy_basic);
@@ -44,6 +105,11 @@ class mainScene {
 
         //Set default state for controls
         this.controlState = "STATE_DEFAULT";
+
+        this.input.keyboard.on("keyup", (event) => {
+            console.dir(event);
+            this.handleInput(event);
+        });
 
         //Temp turn variables
         let actionsTillTurn = 2;
@@ -83,29 +149,27 @@ class mainScene {
     }
 
     update() {
+        this.state.update(this);
         // if the functions are creating the .on() type, then you're creating a new listener on every uipdate cycle
-        switch (this.controlState) {
-            case "STATE_DEFAULT":
-                this.defaultState();
-                break;
-
-            case "STATE_AIMING":
-                if (input == PRESS_DOWN) {
-                    state_ = STATE_DIVING;
-                    setGraphics(IMAGE_DIVE);
-                }
-                break;
-
-            case "STATE_MENU":
-                if (input == RELEASE_DOWN) {
-                    state_ = STATE_STANDING;
-                    setGraphics(IMAGE_STAND);
-                }
-                break;
-            default:
-                console.log("DEFAULT?");
-        }
-
+        // switch (this.controlState) {
+        //     case "STATE_DEFAULT":
+        //         this.defaultState();
+        //         break;
+        //     case "STATE_AIMING":
+        //         if (input == PRESS_DOWN) {
+        //             state_ = STATE_DIVING;
+        //             setGraphics(IMAGE_DIVE);
+        //         }
+        //         break;
+        //     case "STATE_MENU":
+        //         if (input == RELEASE_DOWN) {
+        //             state_ = STATE_STANDING;
+        //             setGraphics(IMAGE_STAND);
+        //         }
+        //         break;
+        //     default:
+        //         console.log("DEFAULT?");
+        // }
         // if (this.physics.overlap(this.player, this.coin)) {
         //     this.hit();
         // }
@@ -122,30 +186,6 @@ class mainScene {
         // }
     }
     defaultState() {
-        // this.input.keyboard.on("keyup-RIGHT", (event) => {
-        //     // this.takeTurn();
-        //     this.player.x += CST.TILE_SIZE;
-        //     console.log(this.player.width, this.player.displayWidth);
-
-        //     console.log("right up!");
-        // });
-        // this.input.keyboard.on("keyup-DOWN", (event) => {
-        //     // this.takeTurn();
-        //     this.player.y += CST.TILE_SIZE;
-
-        //     console.log("down up!");
-        // });
-        // this.input.keyboard.on("keyup-LEFT", (event) => {
-        //     // this.takeTurn();
-        //     this.player.x -= CST.TILE_SIZE;
-        //     console.log("right up!");
-        // });
-        // this.input.keyboard.on("keyup-UP", (event) => {
-        //     // this.takeTurn();
-        //     this.player.y -= CST.TILE_SIZE;
-
-        //     console.log("down up!");
-        // });
         if (this.arrow.up.isDown) {
             // this.takeTurn();
             this.player.x += CST.TILE_SIZE;
@@ -183,69 +223,6 @@ class mainScene {
         //         console.log("Forward Button was released");
         //     }
         // });
-    }
-    /*
-            //Stat Type
-            {
-                Health: 100
-                Defense: X
-                Dodge: X
-                APcharge: 4 // Action points gained per turn
-                MaxAP: 16 //Max amount of Action Points allowed to bank
-                ...
-
-                debuffs: [
-                    //These are SUBTRACTED from stats upon evaluation/use of player stats
-                    //A Bebuff Type would extend the Stat type, but add a ap/turn duration
-                    //these would be added and subtracted, having a action/turn based effect lifetime
-                ]
-                buffs: [
-                    //These are ADDED from stats upon evaluation/use of player stats
-                    //A Buff Type would extend the Stat type, but add a ap/turn duration
-                    //these would be added, having a action/turn based effect lifetime
-                ]
-                mods: [
-                    //these are more permanent effects, not based on time, their effects can be both negative and positive in one mod
-                    //example- Mechanical Arms: give you extra attack damage, but reduce your APrecharge per turn by 1
-                ]
-
-            }
-        */
-
-    // How does initiative work
-    // Enemy turn order is decided like this:
-    // If multiple enemies tie on the number/stat, then they move to the next tie breaker
-    // 1. Initiative base stat
-    // 2. Distance to player
-    // 3. Random roll tie breaker
-
-    // If this doesn't work, consider making it totally random?
-    takeTurn() {
-        // player actions
-        // enemy actions
-        // map moves
-
-        //PLAYER
-        //reset/set AP
-        //spend AP or bank AP/pass
-        // Actions
-        //Move
-        //Check collision
-        //Effect
-        //Use item, spell, attack
-        // Bank
-        // Add remaining AP to bank, no more than max
-        //ENEMY
-
-        //is this a turn?
-
-        if (actionsTillTurn <= 0) {
-            isTurn = true;
-        }
-
-        if (isTurn) {
-            //enemies
-        }
     }
 }
 
